7d0d298e45943c02e79cebcf051e1b4c6f9cf7fc
maxssilva <maxssilva@yahoo.com.br>
2020-06-14 10:07:36 -0300

    biblioteca

---
+++ biblioteca.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+/*
+> maior que
+	< menor do que
+	>= maior ou igual
+	<= menor ou igual
+	==igual a
+	!=diferente de
+
+	int x = 5, y = 3;
+	printf("resultado = %d\n", x > 4);
+	printf("resultado = %d\n", x >= y+2);
+	printf("resultado = %d\n", x  < y);
+	printf("resultado = %d\n", x != y+2);
+	printf("resultado = %d\n", x == y+2);
+
+
+operadores logicos
+
+	E (&&) // ou (||)
+ P	Q 	P e Q	P ou Q
+:)	:)	:)	:)
+
+:)	:(	:(	:)
+
+:(	:)	:(	:)
+
+:(	:(	:(	:(
+
+
+COMANDO IF
+permite executar ou não um conjunto de comandos de acordo com uma condição.
+
+
+OPERADOR TERNÁRIO (?) aula16
+é tipicamente utilizado para fazer atribuiçoes condicinais
+ váriavel =  condição ? expressao1(v) : expressão 2(f)
+
+AULA 17 - COMANDO SWITCH (VARIÁVEL) 
+permite fazer seleções múltiplas com base no valor de uma variável do tipo int ou char, basicamente é um conjunto de "caso", que se houver uma resposta positiva na entrada de dados ele executa a ação, (printf, por ex), e finaliza o switch com o comando break, do contrário ele continua com os outros "casos";
+
+AULA 22 - COMANDO BREAK
+interrompe a repetição de qualquer comando de laço, seua for, while, do-while
+
+AULA 23 - COMANDO CONTINUE
+interrompe a repetição atual de qualquer comando de laço, e passa para a próxima repetição.
+
+AULA 25 - ARRAY / VETORES
+conjunto de variáveis do mesmo tipo usando o mesmo nome
+
+AULA 26-27 ARRAY COM MAIS DIMENSÕES
+
+
+*/
+
+
+
+
+return 0;
+}
+
